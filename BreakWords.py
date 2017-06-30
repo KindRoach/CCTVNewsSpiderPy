3b08104335acb57bb5b1d4dcff084ed139de1870
KindRoach <kindroach@hotmail.com>
2017-06-30 13:30:26 +0800

    添加超时与重试机制，优化 分词结果

--- BreakWords.py
+++ BreakWords.py
@@ -12,6 +12,8 @@ for file in os.listdir(news_path):
     with open(os.path.join(news_path, file), 'r', encoding="utf-8") as in_f:
         with open(os.path.join(words_path, file), 'w', encoding="utf-8") as out_f:
             for line in in_f.readlines():
+                if len(line) < 2:
+                    continue
                 # 过滤所有非中文字符
                 p = re.compile(r"[\u4e00-\u9fa5]")
                 res = re.findall(p, line)
@@ -20,5 +22,6 @@ for file in os.listdir(news_path):
                 words = jieba.cut(result)
                 for word in words:
                     out_f.write(word + " ")
+                out_f.write("\n")
             count += 1
             print("{}/{} done.".format(count, file_num))
--- NewsSpider.py
+++ NewsSpider.py
@@ -1,40 +1,51 @@
 import bs4
-import time
 import requests
 
 news_pages = list()
+timeout_set = 2
 
-# next_uri = "http://mrxwlb.com/category/mrxwlb-text/"
-next_uri = "http://mrxwlb.com/category/mrxwlb-text/page/50/"
+next_uri = "http://mrxwlb.com/category/mrxwlb-text/"
 while next_uri != "":
-    res_str = requests.get(next_uri).content.decode("utf-8")
-    soup = bs4.BeautifulSoup(res_str, "html.parser")
-    for h1 in soup.find_all("h1", class_="entry-title"):
-        news_pages.append(h1.a["href"])
-    next_page_elemnts = soup.find_all("a", class_="next page-numbers")
-    if len(next_page_elemnts) > 0:
-        next_uri = next_page_elemnts[0]["href"]
-    else:
-        next_uri = ""
-    print("{} news page uri collected!".format(len(news_pages)))
-
-correct_num = 0
-error_num = 0
-erroUris = list()
-
-for page in news_pages:
     try:
-        res_str = requests.get(page).content.decode("utf-8")
+        res_str = requests.get(
+            next_uri, timeout=timeout_set).content.decode("utf-8")
+        soup = bs4.BeautifulSoup(res_str, "html.parser")
+        for h1 in soup.find_all("h1", class_="entry-title"):
+            news_pages.append(h1.a["href"])
+        next_page_elemnts = soup.find_all("a", class_="next page-numbers")
+        if len(next_page_elemnts) > 0:
+            next_uri = next_page_elemnts[0]["href"]
+        else:
+            next_uri = ""
+        print("{} news page uri collected!".format(len(news_pages)))
+    except Exception as e:
+        print("Retry.")
+        continue
+
+finish_num = 0
+total_num = len(news_pages)
+news_pages.append("")
+news_pages = (page for page in news_pages)
+next_uri = next(news_pages)
+
+while next_uri != "":
+    try:
+        res_str = requests.get(next_uri, timeout=timeout_set).content.decode("utf-8")
         soup = bs4.BeautifulSoup(res_str, "html.parser")
         content = str()
+
         for p in soup.find_all("p"):
-            content += '\r' + p.text
-        correct_num += 1
-        news_date = soup.find_all("time", class_="entry-date")[0].text
+            if len(p.find_all("strong")) > 0:
+                content += "\r\n\r\n"
+            else:
+                content += p.text
+        finish_num += 1
+        news_date = soup.find_all("h1", class_="entry-title")[0].text
+        news_date = news_date[news_date.find("2"):news_date.find("日") + 1]
         with open(r"data/news/" + news_date + ".txt", 'w', encoding="utf-8") as out_f:
             out_f.write(content)
+        next_uri = next(news_pages)
+        print("{}/{} news done!".format(finish_num, total_num))
     except Exception as e:
-        erroUris.append(page)
-        error_num += 1
-    finally:
-        print("success/error/total:{}/{}/{}".format(correct_num, error_num, len(news_pages)))
+        print("Retry.")
+        continue
